d74541afa4d9cecf8d6c0428ef6d691d2803e87e
Kratos <64226667+thelifeQuest@users.noreply.github.com>
2020-06-24 13:27:12 +0200

    New files

---
+++ sucekr.js
@@ -0,0 +1,32 @@
+
+
+
+
+function logCheck(){
+     var Name=document.getElementById("userName").value;
+
+    var Password=document.getElementById("passWord").value;
+
+
+if(Name=="Juicy" && Password=="mydick"){
+
+   alert("Motherfucker, You're in!");
+
+} else{
+    alert ("Credentials invalid");
+    
+}
+
+
+
+}   
+
+function passReset(){
+document.getElementById("passReset").innerHTML="Your new password is too short, what did you put in??"
+
+
+}
+
+function roboCheck(){
+alert("No, No, You're not a robot, you're just a failure.");
+}
